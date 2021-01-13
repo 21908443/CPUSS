3cd6ca60daf45d5f9f212e3ea4392f16f4e2e3e9
21908443 <76862609+21908443@users.noreply.github.com>
2021-01-13 12:42:59 +0200

    Update running.cpp

--- running.cpp
+++ running.cpp
@@ -17,44 +17,43 @@
 int seconds=0;
 int num;
 queue<Proc> Object;
-//----------------------------------------------------------FCFS implementation-----------------------------------------------------------------------------------
 
 
-void FCFS_implement(Proc& temp)										//function for implementing FCFS algorithm
+void FCFS_implement(Proc& temp)	
 {
-	int flag;											//this will tell the ready process when to send the next Proc
+	int flag;
 	cout<<"We recieved "<<temp.Proc_num<<" in RUNNING at seconds="<<seconds<<" with rem time="<<temp.rem_time<<"\n";
-	while (temp.rem_time >0)//(int i=0; i<5; i++)					//one second will pass in each iteration
+	while (temp.rem_time >0)
 	{
-		sleep(1);								//running the clock
+		sleep(1);
 		seconds++;
 		temp.exit_time++;
-		temp.rem_time--;					//with each second remaining execution time of current process will decrease
-		if (temp.rem_time <= 0)					//if remaining execution time has ended then Proc will go to exit process 
+		temp.rem_time--;
+		if (temp.rem_time <= 0)	
 		{
 			cout<<temp.Proc_num<<" will now go to EXIT state at seconds="<<seconds<<"\n";
 			temp.exit_time=seconds;
 			int pipe5_opener = open("run_to_exit", O_WRONLY);
 			write(pipe5_opener, &temp, sizeof(temp));
 			close(pipe5_opener);
-			flag=1;									//we will also have to tell ready process that now send the next Proc
-			int signaler = open("signal_catcher", O_WRONLY);			//as proc has gone to blocked so sending dispatch signal
+			flag=1;	
+			int signaler = open("signal_catcher", O_WRONLY);			
 			write(signaler, &flag, sizeof( flag ));
 			close(signaler);
 			flag=0;
 			return;
 		}
-		if (seconds % 5 == 0)							//check for I/O after every 5 seconds
+		if (seconds % 5 == 0)	
 		{
-			if ( (rand()%2) == 0)						//random number 0 will mean that TAKE I/O
+			if ( (rand()%2) == 0)	
 			{
 				cout<<temp.Proc_num<<" will now go to blocked state at seconds="<<seconds<<"\n";
-				temp.iotype= (rand() % 3)+1;				//determing which type of I/O
+				temp.iotype= (rand() % 3)+1;
 				int pipe4_opener = open("run_to_block", O_WRONLY);
 				write(pipe4_opener, &temp, sizeof(temp));
 				close(pipe4_opener);
 				flag=1;
-				int signaler = open("signal_catcher", O_WRONLY);		//as proc has gone to blocked so sending dispatch signal
+				int signaler = open("signal_catcher", O_WRONLY);
 				write(signaler, &flag, sizeof( flag ));
 				close(signaler);
 				flag=0;
@@ -64,19 +63,17 @@ void FCFS_implement(Proc& temp)										//function for implementing FCFS algori
 	}
 }
 
-//-------------------------------------------------------------RR implementation--------------------------------------------------------------------------------
-
 void RR_implement(Proc& temp)
 {
 	int flag;
 	cout<<"We recieved "<<temp.Proc_num<<" in RUNNING at seconds="<<seconds<<" with rem time="<<temp.rem_time<<"\n";
-	for(int i=0; i<temp.quantum; i++)					//one second will pass in each iteration
+	for(int i=0; i<temp.quantum; i++)					
 	{
 		sleep(1);
-		seconds++;						//running clock
+		seconds++;						
 		temp.exit_time++;
-		temp.rem_time--;					//with each second remaining execution time of current process will decrease
-		if (temp.rem_time <= 0)					//if remaining execution time is 0 then Proc should go to exit process
+		temp.rem_time--;					
+		if (temp.rem_time <= 0)	
 		{
 			cout<<temp.Proc_num<<" will now go to EXIT state at seconds="<<seconds<<"\n";
 			temp.exit_time=seconds;
@@ -122,7 +119,6 @@ void RR_implement(Proc& temp)
 		}
 	}
 }
-//-----------------------------------------------------------SJF implementation-----------------------------------------------------------------------------------------
 void SJF_implement(Proc& temp)
 {
 	int flag;
@@ -132,8 +128,8 @@ void SJF_implement(Proc& temp)
 		sleep(1);
 		seconds++;
 		temp.exit_time++;
-		temp.rem_time--;					//with each second remaining execution time of current process will decrease
-		if (temp.rem_time <= 0)					//if remaining execution time has ended dispatch next Proc
+		temp.rem_time--;
+		if (temp.rem_time <= 0)	
 		{
 			cout<<temp.Proc_num<<" will now go to EXIT state at seconds="<<seconds<<"\n";
 			temp.exit_time=seconds;
@@ -141,23 +137,23 @@ void SJF_implement(Proc& temp)
 			write(pipe5_opener, &temp, sizeof(temp));
 			close(pipe5_opener);
 			flag=1;
-			int signaler = open("signal_catcher", O_WRONLY);			//as proc has gone to blocked so sending dispatch signal
+			int signaler = open("signal_catcher", O_WRONLY);
 			write(signaler, &flag, sizeof( flag ));
 			close(signaler);
 			flag=0;
 			return;
 		}
-		if (seconds % 5 == 0)							//check for I/O after every 5 seconds
+		if (seconds % 5 == 0)
 		{
 			if ( (rand()%2) == 0)
 			{
 				cout<<temp.Proc_num<<" will now go to blocked state at seconds="<<seconds<<"\n";
-				temp.iotype= (rand() % 3)+1;				//determing the type of I/O
+				temp.iotype= (rand() % 3)+1;
 				int pipe4_opener = open("run_to_block", O_WRONLY);
 				write(pipe4_opener, &temp, sizeof(temp));
 				close(pipe4_opener);
 				flag=1;
-				int signaler = open("signal_catcher", O_WRONLY);		//as proc has gone to blocked so sending dispatch signal
+				int signaler = open("signal_catcher", O_WRONLY);
 				write(signaler, &flag, sizeof( flag ));
 				close(signaler);
 				flag=0;
@@ -166,20 +162,7 @@ void SJF_implement(Proc& temp)
 		}
 	}
 }
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-/*void SRTF(Proc& temp)
-{
-	int flag;
-	cout<<"We recieved "<<temp.Proc_num<<" in RUNNING at seconds="<<seconds<<" with rem time="<<temp.rem_time<<"\n";
-	while (1)
-	{
-		sleep(1);
-		seconds++;
-		P2.rem_time--;
-		P2.exit_time++;
-	}
-}*/
 using namespace std;
 int main()
 {
@@ -215,14 +198,13 @@ int main()
 		else
 		{
 			seconds = P2.exit_time;
-			if (P2.algo[0] == 'F')							//only FCFC starts with F
+			if (P2.algo[0] == 'F')							
 				FCFS_implement(P2);	
-			else if (P2.algo[0]=='R')						//Only RR starts with RR
+			else if (P2.algo[0]=='R')
 				RR_implement(P2);
-			else if (P2.algo[0]=='S' /*&& P2.algo[1]=='J'*/)
+			else if (P2.algo[0]=='S')
 				SJF_implement(P2);
-/*			else if(P2.algo[0]=='S' && P2.algo[1]=='R')
-				SRTF_implement(P2);*/
+
 						
 	    	}
 	}	    	
